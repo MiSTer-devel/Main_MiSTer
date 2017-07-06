7ae9aa6f6f43237954d1b66e4acce7c307fa1f0c
sorgelig <pour.garbage@gmail.com>
2017-07-07 03:28:50 +0800

    HDMI Audio 48khz option.

--- user_io.h
+++ user_io.h
@@ -108,6 +108,7 @@
 #define CONF_CSYNC              0x08
 #define CONF_FORCED_SCANDOUBLER 0x10
 #define CONF_YPBPR              0x20
+#define CONF_AUDIO_48K          0x40
 
 // core type value should be unlikely to be returned by broken cores
 #define CORE_TYPE_UNKNOWN   0x55
