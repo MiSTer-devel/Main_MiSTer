dc97f19c9b5878cb77ec47e44699411698e67b12
Alan Steremberg <alan.steremberg@gmail.com>
2019-11-07 13:27:37 -0800

    added structure and start/length

--- support/arcade/romutils.cpp
+++ support/arcade/romutils.cpp
@@ -16,6 +16,7 @@
  *
  * 00 01 02
  * 000102
+ * 00 01 2 03
  *
  * and return an array and a length of binary values
  *
@@ -92,20 +93,26 @@ user_io.h:int user_io_file_tx_finish();
 */
 
 
+#define kBigTextSize 4096
 struct arc_struct {
-	
+	char zipname[kBigTextSize];
+	char partname[kBigTextSize];
+	char dtdt[kBigTextSize];
+	int offset;
+	int length;
+	buffer_data *data;
 };
 
 /*
  * AJS - TODO:
- *  create  a structure we can pass into send_rom
- *  use dynamic allocation (realloc?) to allocate the bigtext
- *  make sure we push / pop the stack for file names, etc
- *  make sure that merged / non merged roms work
+ *  DONE -- create  a structure we can pass into send_rom
+ *  DONE -- use dynamic allocation (realloc?) to allocate the bigtext
+ *  -- make sure we push / pop the stack for file names, etc
+ *  -- make sure that merged / non merged roms work
  *
  *  ???? -- SORG -- make one big array, and send it to the _tx code as one shot, we can check the MD5 and put up warning as well easily
  *  DONE -- SORG -- make the HEX look like a hex dump so it is easy to edit
- *  SORG -- get rid off uudecode stuff
+ *  DONE -- SORG -- get rid off uudecode stuff
  *  SORG -- part - offset / length get rid of action
  *  AJS = Here are some of the things: Mame Roms, Mame Merged Roms, Binary Sound Files, repeated chunks. Partial Rom Chunks, Flipped Rom Chunks, games that use two mame zips (maybe?), patches, extra data sitting in the releases directory
  */
@@ -113,54 +120,54 @@ struct arc_struct {
 
 static int xml_send_rom(XMLEvent evt, const XMLNode* node, SXML_CHAR* text, const int n, SAX_Data* sd)
 {
-#define kBigTextSize 4096
-	static char zipname[kBigTextSize];
-	static char partname[kBigTextSize];
-	static char bigtext[kBigTextSize];
-	static char action[kBigTextSize];
-	static char dtdt[kBigTextSize];
-	//zipname[0]=0;
+	struct arc_struct *arc_info = (struct arc_struct *)sd->user;
         (void)(sd);
 
         switch (evt)
         {
         case XML_EVENT_START_NODE:
-		bigtext[0]=0;
-		action[0]=0;
+		buffer_destroy(arc_info->data);
+		arc_info->data=buffer_init(kBigTextSize);
+		arc_info->offset=0;
+		arc_info->length=-1;
 		printf("XML_EVENT_START_NODE: tag [%s]\n",node->tag);
                 for (int i = 0; i < node->n_attributes; i++)
                         {
 			   printf("attribute %d name [%s] value [%s]\n",i,node->attributes[i].name,node->attributes[i].value);
 			   if (!strcasecmp(node->attributes[i].name,"zip") && !strcasecmp(node->tag,"rom"))
 			   {
-				   strcpy(zipname,node->attributes[i].value);
-				   printf("found zip: [%s]\n",zipname);
+				   strcpy(arc_info->zipname,node->attributes[i].value);
+				   printf("found zip: [%s]\n",arc_info->zipname);
 			   }
 			   if (!strcasecmp(node->attributes[i].name,"name") && !strcasecmp(node->tag,"rom"))
 			   {
 				   user_io_file_tx_start(node->attributes[i].value);
 			   }
-			   if (!strcasecmp(node->attributes[i].name,"action") && !strcasecmp(node->tag,"part"))
-			   {
-				   strcpy(action,node->attributes[i].value);
-			   }
 			   if (!strcasecmp(node->attributes[i].name,"zip") && !strcasecmp(node->tag,"part"))
 			   {
-				   strcpy(zipname,node->attributes[i].value);
+				   strcpy(arc_info->zipname,node->attributes[i].value);
 			   }
 			   if (!strcasecmp(node->attributes[i].name,"name") && !strcasecmp(node->tag,"part"))
 			   {
-				   strcpy(partname,node->attributes[i].value);
+				   strcpy(arc_info->partname,node->attributes[i].value);
 			   }
 			   if (!strcasecmp(node->attributes[i].name,"dt:dt") && !strcasecmp(node->tag,"part"))
 			   {
-				   strcpy(dtdt,node->attributes[i].value);
+				   strcpy(arc_info->dtdt,node->attributes[i].value);
+			   }
+			   if (!strcasecmp(node->attributes[i].name,"offset") && !strcasecmp(node->tag,"part"))
+			   {
+				   arc_info->offset=atoi(node->attributes[i].value);
+			   }
+			   if (!strcasecmp(node->attributes[i].name,"length") && !strcasecmp(node->tag,"part"))
+			   {
+				   arc_info->length=atoi(node->attributes[i].value);
 			   }
                         }
 
                 break;
         case XML_EVENT_TEXT:
-		strncat(bigtext,text,kBigTextSize-strlen(bigtext)-1);
+		buffer_append(arc_info->data, text);
 		printf("XML_EVENT_TEXT: text [%s]\n",text);
 		break;
         case XML_EVENT_END_NODE:
@@ -172,40 +179,29 @@ static int xml_send_rom(XMLEvent evt, const XMLNode* node, SXML_CHAR* text, cons
 		//int user_io_file_tx_body_filepart(const char *name,int start, int len)
 		if (!strcasecmp(node->tag,"part")) 
 		{
-			char fname[4096];
-			printf("bigtext [%s]\n",bigtext);
-			printf("partname[%s]\n",partname);
-			printf("zipname [%s]\n",zipname);
-			sprintf(fname,"arcade/mame/%s/%s",zipname,partname);
+			char fname[kBigTextSize*2+16];
+			int start,length;
+			start=arc_info->offset;
+			length=0;
+			if (length>0) length = arc_info->length;
+			printf("data[%s]\n",arc_info->data->content);
+			printf("partname[%s]\n",arc_info->partname);
+			printf("zipname [%s]\n",arc_info->zipname);
+			printf("offset[%d]\n",arc_info->offset);
+			printf("length[%d]\n",arc_info->length);
+			sprintf(fname,"arcade/mame/%s/%s",arc_info->zipname,arc_info->partname);
 			//user_io_file_tx_body_filepart(getFullPath(fname),0,0);
-			if (strlen(partname)) {
-			        printf("user_io_file_tx_body_filepart(const char *name[%s],int start[], int len[])\n",fname);
-				user_io_file_tx_body_filepart(fname,0,0);
+			if (strlen(arc_info->partname)) {
+			        printf("user_io_file_tx_body_filepart(const char *name[%s],int start[%d], int len[%d])\n",fname,start,length);
+				user_io_file_tx_body_filepart(fname,start,length);
 			}
 			else // we have binary data?
 			{
-				if (!strcasecmp(dtdt,"bin.base64"))
+				if (!strcasecmp(arc_info->dtdt,"bin.hex")) 
 				{
-					size_t i,size,r_size;
-					printf("we have bin.base64 data [%s]\n",bigtext);
-					size = strlen(bigtext);
-	                                unsigned char *decoded = base64simple_decode(bigtext, size, &r_size);
-	                                if (decoded == NULL) {
-		                            printf("Improperly Encoded String or Insufficient Memory!\n");
-	                                } else {
-		                             for (i = 0; i < r_size; ++i) {
-				               printf(" %d (%c)",decoded[i],decoded[i]);
-			                      // Do something with decoded[i] here
-		                             }
-				             user_io_file_tx_body((const uint8_t*)decoded,size);
-	                                }
-	                                if (decoded) free(decoded);
-				}
-				else if (!strcasecmp(dtdt,"bin.hex")) 
-				{
-					printf("we have bin.hex data [%s]\n",bigtext);
+					printf("we have bin.hex data [%s]\n",arc_info->data->content);
 					size_t len=0;
-					unsigned char* binary=hexstr_to_char(bigtext,&len);
+					unsigned char* binary=hexstr_to_char(arc_info->data->content,&len);
 					printf("len %d:\n",len);
 					for (size_t i=0;i<len;i++) {
 						printf(" %d ",binary[i]);
@@ -289,14 +285,17 @@ int arcade_send_rom(const char *xml)
         sax.all_event = xml_send_rom;
 
 	// grab the file size so we can pre-allocate buffers for inline data
-        struct stat st;
-        stat(xml, &st);
-        off_t size = st.st_size;
+        //struct stat st;
+        //stat(xml, &st);
+        //off_t size = st.st_size;
 
 	// create the structure we use for the XML parser
+	struct arc_struct arc_info;
+	arc_info.data=buffer_init(kBigTextSize);
 
 	// parse
-        XMLDoc_parse_file_SAX(xml, &sax, 0);
+        XMLDoc_parse_file_SAX(xml, &sax, &arc_info);
+	buffer_destroy(arc_info.data);
         return 0;
 }
 
