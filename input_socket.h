9c7117107be6533f2854ef3526949f281f7aa395
Jayson Larose <jayson@interlaced.org>
2022-05-04 06:44:30 -0700

    * Actually included the files instead of symlinking them this time

--- input_socket.h
+++ input_socket.h
@@ -1,1 +1,14 @@
-/home/jayson/bin/projects/mister_input_socket/input_socket.h
+#ifndef __INPUT_SOCKET_H
+#define __INPUT_SOCKET_H
+
+#include <linux/input.h>
+#include <stdint.h>
+#include "input.h"
+
+
+void input_socket_init(void);
+void input_socket_send(uint8_t, struct input_event *i, devInput *);
+int input_socket_poll(int);
+void input_socket_destroy(void);
+
+#endif
