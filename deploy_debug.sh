7c6bf7248ef64d875467ff85eb7c7f17ff271ba3
Martin Donlon <github-martin@donlons.com>
2022-05-05 21:28:49 -0700

    Kill mister.elf process Associate task problems with cpp extension

--- deploy_debug.sh
+++ deploy_debug.sh
@@ -6,6 +6,6 @@ REMOTEPATH=/media/fat/MiSTer.elf
 EXECUTABLE=MiSTer.elf
 
 # remove old executable on target
-ssh root@$TARGET rm $REMOTEPATH
+ssh root@$TARGET "rm $REMOTEPATH; killall -q MiSTer.elf"
 # copy over new executable
 scp $EXECUTABLE root@$TARGET:$REMOTEPATH
