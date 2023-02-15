52237c4d36cf69f87d38de8a72c9f9784c328de2
Erik Scheffers <eriks5@users.noreply.github.com>
2023-02-15 15:49:01 +0100

    C128: Extend C64 disk routines to support 1571 drive (dual-sided disks and MFM formats) (#742)
    * C128: add support for .d71 and .g71 disk images
    
    * C128: add creating or resizing tracks in .g64/.g71 images
    
    * C128: change "1571" flag from bit 8 of lba to bit 10
    
    * C128: fix second side of d71 images
    
    * C128: small fix for gcr track end position detection
    
    * C128: set track speed to 8 for MFM tracks
    
    * C128: provide detected disk image type to core
    
    * C128: add bit to indicate disk image GCR support

--- support/c64/c64.h
+++ support/c64/c64.h
@@ -1,12 +1,20 @@
 #ifndef C64_H
 #define C64_H
 
+#define G64_BLOCK_COUNT_1541 31
+#define G64_BLOCK_COUNT_1571 52
+
+#define G64_SUPPORT_DS    1
+#define G64_SUPPORT_GCR   2
+#define G64_SUPPORT_MFM   4
+#define G64_SUPPORT_HD    8
+
 int c64_openT64(const char *path, fileTYPE* f);
 
 int c64_openGCR(const char *path, fileTYPE *f, int idx);
 void c64_closeGCR(int idx);
 
-void c64_readGCR(int idx, uint8_t track);
-void c64_writeGCR(int idx, uint8_t track);
+void c64_readGCR(int idx, uint64_t lba, uint32_t blks);
+void c64_writeGCR(int idx, uint64_t lba, uint32_t blks);
 
 #endif
