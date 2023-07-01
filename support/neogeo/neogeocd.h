71a8caacde94dd4ba31eea4a91b776f7f5a0a25a
paulb-nl <boumanpaul@gmail.com>
2023-07-01 10:53:18 +0200

    NeoGeo CD updates (#787)
    - Add Unibios and CDZ support
    - Ask FPGA before sending sector
    - 1-4x CD speed

--- support/neogeo/neogeocd.h
+++ support/neogeo/neogeocd.h
@@ -6,6 +6,8 @@ void neocd_poll();
 void neocd_set_image(char *filename);
 void neocd_reset();
 int neocd_send_data(uint8_t* buf, int len, uint8_t index);
+int neocd_can_send_data(uint8_t type);
 int neocd_is_en();
 void neocd_set_en(int enable);
+void set_poll_timer();
 #endif
