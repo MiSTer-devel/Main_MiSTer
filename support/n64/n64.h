03b4e0097c5948f36bce83b12f9f92bf86814dfc
Rikard Bengtsson <36540711+yxkalle@users.noreply.github.com>
2024-01-16 13:24:40 +0100

    N64: Hide auto-detect info with MiSTer.ini (#868)

--- support/n64/n64.h
+++ support/n64/n64.h
@@ -2,6 +2,8 @@
 #define N64_H
 
 #include <stdint.h>
+#include <stdio.h>
+#include "../../cfg.h"
 #include "../../file_io.h"
 
 int n64_rom_tx(const char* name, unsigned char index, uint32_t load_addr);
