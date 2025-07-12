d37f1a096490e99179e2d906b310fc1d0c46f505
Zakk <zakk@rsdio.com>
2025-07-12 00:31:25 -0400

    Bumper l/r or -/+ -> prev/next letter in file picker

--- file_io.h
+++ file_io.h
@@ -63,6 +63,8 @@ char* flist_GetPrevNext(const char* base_path, const char* file, const char* ext
 #define SCANF_PREV_PAGE -2 // find previous 16 files in directory
 #define SCANF_SET_ITEM   3 // find exact item
 #define SCANF_END        4 // find last file in directory
+#define SCANF_NEXT_CHAR  5 // find next letter/number
+#define SCANF_PREV_CHAR -5 // find previous letter/number
 
 // options flags
 #define SCANO_DIR        0b000000001 // include subdirectories
