MiSTer-devel/Main_MiSTer

commit c0171c9dcaa24f7d93916234a766b72c460a3424

CD-i: Fixed buffer overflow (#996)
Fixes potential cause of SIGSEGV
during image reading

support/cdi/cdi.cpp
@@ -682,7 +682,10 @@ void subcode_data(int lba, struct subcode &out)
 
 		int track = toc.GetTrackByLBA(lba + 150);
 
-		int track_lba = lba - toc.tracks[track].start;
+		int track_lba = 0;
+		if (track < (int)ARRAY_LENGTH(toc.tracks))
+			track_lba = lba - toc.tracks[track].start;
+
 		int index = 1;
 
 		if (track_lba < 0)
@@ -699,7 +702,8 @@ void subcode_data(int lba, struct subcode &out)
 		ts = track_lba / 75;
 		tf = track_lba % 75;
 
-		out.control = htons(toc.tracks[track].type ? 0x41 : 0x01);
+		if (track < (int)ARRAY_LENGTH(toc.tracks))
+			out.control = htons(toc.tracks[track].type ? 0x41 : 0x01);
 		out.track = htons(BCD(track + 1));
 		out.index = htons(BCD(index));
 		out.mode1_mins = htons(BCD(tm));
@@ -812,9 +816,7 @@ void cdi_read_cd(uint8_t *buffer, int lba, int cnt)
 			}
 		}
 
-		check_scramble(lba, buffer);
 		buffer += CD_SECTOR_LEN;
-		subcode_data(lba, *reinterpret_cast<struct subcode *>(buffer));
 		buffer += sizeof(struct subcode);
 		cnt--;
 		lba++;

commit 6a5c4a3e5ca5ccdf4001804bdda0671ba9ecf182

CD-i: Fix table of contents
Repair of accidental removal of subcode generation
during Lead-In. (negative lba)

Bug was introduced with c0171c9

support/cdi/cdi.cpp
@@ -657,6 +657,8 @@ void subcode_data(int lba, struct subcode &out)
 		as = rem_lba / 75;
 		af = rem_lba % 75;
 
+		if (toc_entry_count == 0) // catch division by zero
+			return;
 		auto &toc_entry = toc_buffer[lba % toc_entry_count];
 
 		out.control = htons(toc_entry.control);
@@ -817,6 +819,7 @@ void cdi_read_cd(uint8_t *buffer, int lba, int cnt)
 		}
 
 		buffer += CD_SECTOR_LEN;
+		subcode_data(lba, *reinterpret_cast<struct subcode *>(buffer));
 		buffer += sizeof(struct subcode);
 		cnt--;
 		lba++;