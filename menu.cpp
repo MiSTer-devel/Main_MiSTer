69cbd523e7171edaa122342a3a996e958ba4abb5
Sidnei Vladisauskis <sidneivl@gmail.com>
2026-01-09 15:51:07 -0300

    format

--- menu.cpp
+++ menu.cpp
@@ -45,6 +45,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <hci.h>
 #include <hci_lib.h>
 
+#include "cdrom_io.h"
 #include "file_io.h"
 #include "osd.h"
 #include "hardware.h"
@@ -65,7 +66,67 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "ide.h"
 #include "profiling.h"
 
+#include "video.h"
 /*menu states*/
+#include <dirent.h>
+
+static void FindLatestCore(const char* core_name, char* out_path) {
+	const char* paths[] = { "/media/fat/_Console", "/media/fat/_Computer", "/media/fat", NULL };
+	char latest_file[256];
+	latest_file[0] = 0;
+	out_path[0] = 0;
+
+	for (int i = 0; paths[i]; i++) {
+		DIR *dir = opendir(paths[i]);
+		if (!dir) continue;
+
+		struct dirent *ent;
+		while ((ent = readdir(dir)) != NULL) {
+			if (ent->d_type == DT_REG || ent->d_type == DT_LNK) {
+				if (strncmp(ent->d_name, core_name, strlen(core_name)) == 0) {
+					char *ext = strrchr(ent->d_name, '.');
+					if (ext && strcasecmp(ext, ".rbf") == 0) {
+						if (strcmp(ent->d_name, latest_file) > 0) {
+							strcpy(latest_file, ent->d_name);
+							sprintf(out_path, "%s/%s", paths[i], ent->d_name);
+						}
+					}
+				}
+			}
+		}
+		closedir(dir);
+	}
+}
+
+void AutoLoadCore(DiscType type) {
+	const char* core_name = nullptr;
+	switch(type) {
+		case DISC_MEGACD: core_name = "MegaCD"; break;
+		case DISC_SATURN: core_name = "Saturn"; break;
+		case DISC_PSX:    core_name = "PSX"; break;
+		case DISC_NEOGEO: core_name = "NeoGeo"; break;
+		default: return;
+	}
+
+	if (core_name) {
+		char rbf_path[1024];
+		FindLatestCore(core_name, rbf_path);
+
+		if (rbf_path[0]) {
+			printf("[AutoLoad] Loading core: %s\n", rbf_path);
+			char msg[64];
+			sprintf(msg, "Loading %s...", core_name);
+			OsdWrite(15, msg, 3, 0);
+			fpga_load_rbf(rbf_path);
+		} else {
+			printf("[AutoLoad] Core not found: %s\n", core_name);
+			char msg[64];
+			sprintf(msg, "%s Core Not Found!", core_name);
+			OsdWrite(15, msg, 3, 0);
+		}
+	}
+}
+
 enum MENU
 {
 	MENU_NONE1,
@@ -6961,6 +7022,21 @@ void HandleUI(void)
 				if (getNet(2)) str[n++] = 0x1d;
 				if (getNet(1)) str[n++] = 0x1c;
 				if (hci_get_route(0) >= 0) str[n++] = 4;
+				if (hasCDROMMedia(0) || hasCDROMMedia(1) || hasCDROMMedia(2) || hasCDROMMedia(3)) {
+					str[n++] = 0x98;
+					static int last_auto_load_index = -1;
+					static DiscType last_auto_load_type = DISC_UNKNOWN;
+					for(int i=0; i<4; i++) {
+						if (hasCDROMMedia(i)) {
+							DiscType type = getCDROMType(i);
+							if (type != DISC_UNKNOWN && (i != last_auto_load_index || type != last_auto_load_type)) {
+								last_auto_load_index = i;
+								last_auto_load_type = type;
+								AutoLoadCore(type);
+							}
+						}
+					}
+				} else if (isCDROMPresent(0) || isCDROMPresent(1) || isCDROMPresent(2) || isCDROMPresent(3)) str[n++] = 0x97;
 				if (user_io_get_sdram_cfg() & 0x8000)
 				{
 					switch (user_io_get_sdram_cfg() & 7)
