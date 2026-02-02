5ea051dcea8e29ef9a61e4b732c004e724a30aa4
Tony Toon <rtoon@ivytech.edu>
2026-02-02 00:42:10 -0600

    input: switch autofire to tracking frames per key (#1094)

--- autofire.h
+++ autofire.h
@@ -7,8 +7,8 @@ const char *get_autofire_rate_hz(int player, uint32_t code);
 bool is_autofire_enabled(int player, uint32_t code);
 void clear_autofire(int player);
 void inc_autofire_code(int player, uint32_t code, uint32_t mask);
-void autofire_tick();
+//void autofire_tick();
 bool parse_autofire_cfg();
-bool get_autofire_bit(int player, uint32_t code);
+bool get_autofire_bit(int player, uint32_t code, uint32_t frame_count);
 
 #endif
